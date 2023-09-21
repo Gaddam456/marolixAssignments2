c6c4dc1c488450ee815195eebef5fb8f40e5dece
unknown <rajashekhargaddam.marolix@gmail.com>
2023-09-21 18:33:19 +0530

    Two files are added

---
+++ conditionsS.py
@@ -0,0 +1,5 @@
+a = 21
+if a > 20:
+    print("Number is greater than twenty")
+else:
+    print("Number is less than twenty")
---
+++ nested_conditions.py
@@ -0,0 +1,7 @@
+Rajesh_age = 25
+Arun_age = 24
+if Rajesh_age > 18:
+    if Arun_age > 18:
+        print("Both Rajesh and Arun are eligible to vote")
+    else:
+        print("Only Rajesh is eligible to vote")
